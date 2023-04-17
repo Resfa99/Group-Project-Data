36f2f04b00ea3bc34dbdfbc9e35f58acc8cb9b29
Resfa99 <63654981+Resfa99@users.noreply.github.com>
2023-04-17 14:35:36 +0200

    Update Merging-Cleaning.py
    Removing respondents outside the age range (Q13)

--- Merging-Cleaning.py
+++ Merging-Cleaning.py
@@ -16,5 +16,10 @@ df.insert(loc=49, column='Q72TIME', value=df3.iloc[:, 8:15].sum(axis=1))
 
 df.drop(df.columns[73:87], axis=1, inplace=True)
 
+# Removing respondents outside the age range (Q13)
+df['Q13Age'] = df['Q13Age'].clip(7, 17)
+print(df['Q13Age'].min())
+
+
 print(df)
 df.to_csv('merged.csv', index=False)
