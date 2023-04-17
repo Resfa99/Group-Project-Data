b6ea4d506b901606deeb16d90552bc35f40f27f4
Resfa99 <63654981+Resfa99@users.noreply.github.com>
2023-04-17 14:21:40 +0200

    Add files via upload

---
+++ Merging-Cleaning.py
@@ -0,0 +1,20 @@
+import pandas as pd
+# Importing the data
+df1 = pd.read_csv('dataset1.csv')
+df2 = pd.read_csv('dataset2.csv')
+df3 = pd.read_csv('dataset3.csv')
+
+# Merging the datasets
+merged = pd.merge(df1, df2, on='UQNO')
+df = pd.merge(merged, df3, on='ID')
+
+# Replacing the Q54TIME_DAY and Q72TIME_DAY columns by a single Q54TIME and Q72TIME column
+df.drop(['Q54TIME', 'Q72TIME'], axis=1, inplace=True)
+
+df.insert(loc=41, column='Q54TIME', value=df3.iloc[:, 1:8].sum(axis=1))
+df.insert(loc=49, column='Q72TIME', value=df3.iloc[:, 8:15].sum(axis=1))
+
+df.drop(df.columns[73:87], axis=1, inplace=True)
+
+print(df)
+df.to_csv('merged.csv', index=False)
